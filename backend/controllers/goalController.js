f8b0093d5a9e560473f1253fbf4b5c7e7638e0ef
BarkaouiMohamed <mohamedbarkaoui999@gmail.com>
2022-03-02 15:35:52 +0100

    Goals controller and routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,29 +1,48 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get Goals
 // @route Get/api/goals
 // @access Private
-const getGoals = (req, res) => res.status(200).json({  message: 'Get Goals '}) // you can set the status by yourself
+const getGoals =asyncHandler( async (req, res) => {
+
+    res.status(200).json({
+        message: 'Get -Goals '
+    })
+}) // you can set the status by yourself
 
 // @desc Set Goal
 // @route POST/api/goals
 // @access Private
-const setGoal = (req, res) => res.status(200).json({ 
-    message: 'Set Goal '
-}) 
+const setGoal = asyncHandler(async(req, res) => {
+    if (!req.body.text)
+    /* {res.status(400).json({message: 'Please set a text'})} */
+    {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+    res.status(200).json({
+        message: 'Set goal'
+    })
+} )
 
 // @desc Update Goal
 // @route PUT/api/goals/:id
 // @access Private
-const updateGoal = (req, res) => res.status(200).json({ 
-    message: ` Update Goal ${req.params.id} `
-}) 
+
+    const updateGoal =asyncHandler( async (req, res) => {
+    res.status(200).json({
+        message: ` Update Goal ${req.params.id} `
+    })
+})
 
 // @desc Delete Goal
 // @route Delete/api/goals/:id
 // @access Private
-const deleteGoal = (req, res) => res.status(200).json({
-    message: ` Delete Goal ${req.params.id} `
+const deleteGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: ` Delete Goal ${req.params.id} `
+    })
 })
-
 // to use the getGoals we need to export it 
 module.exports = {
     getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv').config() // that will allow us to have a .env file with our variables in it 
+const {
+    errorHandler
+} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000 // the port which we want our server to run on 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
 // app.get('/api/goals',(req, res)=> res.send('Get goals'))
-app.use('/api/goals',require('./routes/goalRoutes'))
-
-app.listen(port, () => console.log(` server started on port ${port}`) ) // which generate the output  
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(errorHandler)
+app.listen(port, () => console.log(` server started on port ${port}`)) // which generate the output  
